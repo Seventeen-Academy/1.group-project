d180659325df9d5100369e207c48e2205ab2d144
gemakurnia <kurniagema@gmail.com>
2023-05-02 20:32:58 +0700

    add link style and js alert

--- assets/js/script-laporan.js
+++ assets/js/script-laporan.js
@@ -1,3 +1,33 @@
-$(function () {
-  $("#datepicker").datepicker();
+const form = document.getElementById("myform");
+const judulInput = document.getElementById("judul");
+const lokasiInput = document.getElementById("lokasi");
+const tanggalInput = document.getElementById("tanggal");
+const isiInput = document.getElementById("isi");
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+
+  if (judulInput.value.trim() === "") {
+    alert("Judul field is required!");
+    return;
+  }
+
+  if (lokasiInput.value.trim() === "") {
+    alert("Lokasi field is required!");
+    return;
+  }
+
+  if (tanggalInput.value.trim() === "") {
+    alert("Tanggal field is required!");
+    return;
+  }
+
+  if (isiInput.value.trim() === "") {
+    alert("Isi field is required!");
+    return;
+  }
+  // Jika semua field sudah diisi, tampilkan data yang diisi
+  alert(
+    `Judul: ${judulInput.value}\nLokasi: ${lokasiInput.value}\nTanggal: ${tanggalInput.value}\nIsi: ${isiInput.value}`
+  );
 });
